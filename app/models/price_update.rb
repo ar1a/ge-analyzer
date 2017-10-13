970d7f542a3a374380b234d5e421bcb40d4a41f6
aria <aria@ar1as.space>
2017-10-14 01:39:57 +0800

    Updates

--- app/models/price_update.rb
+++ app/models/price_update.rb
@@ -4,7 +4,7 @@ class PriceUpdate < ApplicationRecord
     thing = JSON.parse RestClient.get('https://rsbuddy.com/exchange/summary.json').body
     thing.each do |i|
       i = i[1]
-      next if i['overall_average'].to_i <= 0 
+      next if i['overall_average'].to_i <= 0 && i['buy_average'].to_i <= 0 && i['sell_average'].to_i <= 0
       item = Item.find_or_create_by(runescape_id: i['id'])
       item.update(name: i['name'])
       update = item.price_updates.build
