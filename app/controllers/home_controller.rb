028ae5206e605b1f0bd4b3c0c6f8b41a1b47705e
aria <aria@ar1as.space>
2017-10-15 01:49:22 +0800

    only show random items with a margin

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,15 @@ class HomeController < ApplicationController
   def index
     @q = Item.ransack(params[:q])
     @items = if params[:q].nil?
-               Item.all.sample(5)
+               # Item.all.sample(5)
+               arr = []
+               items = Item.all
+               loop do
+                 break if arr.count >= 5
+                 i = items.sample
+                 arr << i if i.margin > 0
+               end
+               arr
              else
                @search = true
                @q.result
