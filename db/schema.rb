68e020325cc56bd50a033eb21e3e0e5a7b4c8931
aria <aria@ar1as.space>
2017-10-20 13:53:26 +0800

    add storext

--- db/schema.rb
+++ db/schema.rb
@@ -10,155 +10,141 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_171_016_151_332) do
-  create_table 'ahoy_events', force: :cascade do |t|
-    t.integer 'visit_id'
-    t.integer 'user_id'
-    t.string 'name'
-    t.text 'properties'
-    t.datetime 'time'
-    t.index %w[name time], name: 'index_ahoy_events_on_name_and_time'
-    t.index %w[user_id name], name: 'index_ahoy_events_on_user_id_and_name'
-    t.index %w[visit_id name], name: 'index_ahoy_events_on_visit_id_and_name'
+ActiveRecord::Schema.define(version: 20171020055159) do
+
+  create_table "ahoy_events", force: :cascade do |t|
+    t.integer "visit_id"
+    t.integer "user_id"
+    t.string "name"
+    t.text "properties"
+    t.datetime "time"
+    t.index ["name", "time"], name: "index_ahoy_events_on_name_and_time"
+    t.index ["user_id", "name"], name: "index_ahoy_events_on_user_id_and_name"
+    t.index ["visit_id", "name"], name: "index_ahoy_events_on_visit_id_and_name"
   end
 
-  create_table 'blazer_audits', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'query_id'
-    t.text 'statement'
-    t.string 'data_source'
-    t.datetime 'created_at'
-    t.index ['query_id'], name: 'index_blazer_audits_on_query_id'
-    t.index ['user_id'], name: 'index_blazer_audits_on_user_id'
+  create_table "blazer_audits", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "query_id"
+    t.text "statement"
+    t.string "data_source"
+    t.datetime "created_at"
+    t.index ["query_id"], name: "index_blazer_audits_on_query_id"
+    t.index ["user_id"], name: "index_blazer_audits_on_user_id"
   end
 
-  create_table 'blazer_checks', force: :cascade do |t|
-    t.integer 'creator_id'
-    t.integer 'query_id'
-    t.string 'state'
-    t.string 'schedule'
-    t.text 'emails'
-    t.string 'check_type'
-    t.text 'message'
-    t.datetime 'last_run_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['creator_id'], name: 'index_blazer_checks_on_creator_id'
-    t.index ['query_id'], name: 'index_blazer_checks_on_query_id'
+  create_table "blazer_checks", force: :cascade do |t|
+    t.integer "creator_id"
+    t.integer "query_id"
+    t.string "state"
+    t.string "schedule"
+    t.text "emails"
+    t.string "check_type"
+    t.text "message"
+    t.datetime "last_run_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_blazer_checks_on_creator_id"
+    t.index ["query_id"], name: "index_blazer_checks_on_query_id"
   end
 
-  create_table 'blazer_dashboard_queries', force: :cascade do |t|
-    t.integer 'dashboard_id'
-    t.integer 'query_id'
-    t.integer 'position'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['dashboard_id'], name: 'index_blazer_dashboard_queries_on_dashboard_id'
-    t.index ['query_id'], name: 'index_blazer_dashboard_queries_on_query_id'
+  create_table "blazer_dashboard_queries", force: :cascade do |t|
+    t.integer "dashboard_id"
+    t.integer "query_id"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dashboard_id"], name: "index_blazer_dashboard_queries_on_dashboard_id"
+    t.index ["query_id"], name: "index_blazer_dashboard_queries_on_query_id"
   end
 
-  create_table 'blazer_dashboards', force: :cascade do |t|
-    t.integer 'creator_id'
-    t.text 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['creator_id'], name: 'index_blazer_dashboards_on_creator_id'
+  create_table "blazer_dashboards", force: :cascade do |t|
+    t.integer "creator_id"
+    t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_blazer_dashboards_on_creator_id"
   end
 
-  create_table 'blazer_queries', force: :cascade do |t|
-    t.integer 'creator_id'
-    t.string 'name'
-    t.text 'description'
-    t.text 'statement'
-    t.string 'data_source'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['creator_id'], name: 'index_blazer_queries_on_creator_id'
+  create_table "blazer_queries", force: :cascade do |t|
+    t.integer "creator_id"
+    t.string "name"
+    t.text "description"
+    t.text "statement"
+    t.string "data_source"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_blazer_queries_on_creator_id"
   end
 
-  create_table 'items', force: :cascade do |t|
-    t.integer 'runescape_id'
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'icon'
-    t.datetime 'last_update_time'
-    t.decimal 'roi'
-    t.integer 'buying_rate'
-    t.integer 'selling_rate'
-    t.decimal 'recommended_buy_price'
-    t.decimal 'recommended_sell_price'
-    t.integer 'margin'
-    t.index ['buying_rate'], name: 'index_items_on_buying_rate'
-    t.index ['margin'], name: 'index_items_on_margin'
-    t.index ['name'], name: 'index_items_on_name'
-    t.index ['recommended_buy_price'], name: 'index_items_on_recommended_buy_price'
-    t.index ['recommended_sell_price'], name: 'index_items_on_recommended_sell_price'
-    t.index ['roi'], name: 'index_items_on_roi'
-    t.index ['runescape_id'], name: 'index_items_on_runescape_id'
-    t.index ['selling_rate'], name: 'index_items_on_selling_rate'
+  create_table "items", force: :cascade do |t|
+    t.integer "runescape_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "icon"
+    t.datetime "last_update_time"
+    t.decimal "roi"
+    t.integer "buying_rate"
+    t.integer "selling_rate"
+    t.decimal "recommended_buy_price"
+    t.decimal "recommended_sell_price"
+    t.integer "margin"
+    t.index ["buying_rate"], name: "index_items_on_buying_rate"
+    t.index ["margin"], name: "index_items_on_margin"
+    t.index ["name"], name: "index_items_on_name"
+    t.index ["recommended_buy_price"], name: "index_items_on_recommended_buy_price"
+    t.index ["recommended_sell_price"], name: "index_items_on_recommended_sell_price"
+    t.index ["roi"], name: "index_items_on_roi"
+    t.index ["runescape_id"], name: "index_items_on_runescape_id"
+    t.index ["selling_rate"], name: "index_items_on_selling_rate"
   end
 
-  create_table 'price_updates', force: :cascade do |t|
-    t.integer 'buy_average'
-    t.integer 'sell_average'
-    t.integer 'overall_average'
-    t.integer 'item_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.decimal 'roi'
-    t.index ['created_at'], name: 'index_price_updates_on_created_at'
-    t.index ['item_id'], name: 'index_price_updates_on_item_id'
-    t.index ['roi'], name: 'index_price_updates_on_roi'
+  create_table "price_updates", force: :cascade do |t|
+    t.integer "buy_average"
+    t.integer "sell_average"
+    t.integer "overall_average"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.decimal "roi"
+    t.index ["created_at"], name: "index_price_updates_on_created_at"
+    t.index ["item_id"], name: "index_price_updates_on_item_id"
+    t.index ["roi"], name: "index_price_updates_on_roi"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer 'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string 'current_sign_in_ip'
-    t.string 'last_sign_in_ip'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'username'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
-    t.index ['username'], name: 'index_users_on_username', unique: true
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'jsonb' for column 'settings'
+
+  create_table "visits", force: :cascade do |t|
+    t.string "visit_token"
+    t.string "visitor_token"
+    t.string "ip"
+    t.text "user_agent"
+    t.text "referrer"
+    t.text "landing_page"
+    t.integer "user_id"
+    t.string "referring_domain"
+    t.string "search_keyword"
+    t.string "browser"
+    t.string "os"
+    t.string "device_type"
+    t.integer "screen_height"
+    t.integer "screen_width"
+    t.string "country"
+    t.string "region"
+    t.string "city"
+    t.string "postal_code"
+    t.decimal "latitude"
+    t.decimal "longitude"
+    t.string "utm_source"
+    t.string "utm_medium"
+    t.string "utm_term"
+    t.string "utm_content"
+    t.string "utm_campaign"
+    t.datetime "started_at"
+    t.index ["user_id"], name: "index_visits_on_user_id"
+    t.index ["visit_token"], name: "index_visits_on_visit_token", unique: true
   end
 
-  create_table 'visits', force: :cascade do |t|
-    t.string 'visit_token'
-    t.string 'visitor_token'
-    t.string 'ip'
-    t.text 'user_agent'
-    t.text 'referrer'
-    t.text 'landing_page'
-    t.integer 'user_id'
-    t.string 'referring_domain'
-    t.string 'search_keyword'
-    t.string 'browser'
-    t.string 'os'
-    t.string 'device_type'
-    t.integer 'screen_height'
-    t.integer 'screen_width'
-    t.string 'country'
-    t.string 'region'
-    t.string 'city'
-    t.string 'postal_code'
-    t.decimal 'latitude'
-    t.decimal 'longitude'
-    t.string 'utm_source'
-    t.string 'utm_medium'
-    t.string 'utm_term'
-    t.string 'utm_content'
-    t.string 'utm_campaign'
-    t.datetime 'started_at'
-    t.index ['user_id'], name: 'index_visits_on_user_id'
-    t.index ['visit_token'], name: 'index_visits_on_visit_token', unique: true
-  end
 end
