e69cf23b5eb41a347e4cc5b960188142f471d98c
aria <aria@ar1as.space>
2017-11-07 17:59:57 +0800

    set base url for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,8 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
+  config.action_controller.default_url_options = { host: 'https://ar1as.space' }
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
