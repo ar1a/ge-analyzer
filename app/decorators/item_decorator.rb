92ee140bb5e3758868613e04e1bfda12adb90430
aria <aria@ar1as.space>
2017-11-11 11:28:04 +0800

    change chip text to be a lighter green

--- app/decorators/item_decorator.rb
+++ app/decorators/item_decorator.rb
@@ -53,6 +53,6 @@ class ItemDecorator < ApplicationDecorator
   end
 
   def margin_style
-    object.margin != 0 ? 'green-text' : 'red-text'
+    object.margin != 0 ? 'light-green-text text-lighten-1' : 'red-text'
   end
 end
